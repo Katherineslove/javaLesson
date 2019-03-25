3944845f7df1d0b412e2224396740d4ab79ae593
Katherine Love <270010932@bun2-206-16.local>
2019-03-26 09:54:03 +1300

    Java lesson

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("External JS is working");
+$(document).ready(function() {
+      console.log("jQuery has loaded");
+      // $("div").hide();
+      // $("#box2").hide();
+      // $(".box").hide();
+      $("#box4").show();
+
+
+
+
+
+
+
+
+
+
+
+
+})
