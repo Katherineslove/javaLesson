37dced40121de4e716c7e2d09317ec312e1021b8
Katherine Love <270010932@bun2-206-16.local>
2019-03-27 09:44:33 +1300

    Java Lesson Day 2

--- js/script.js
+++ js/script.js
@@ -4,17 +4,36 @@ $(document).ready(function() {
       // $("div").hide();
       // $("#box2").hide();
       // $(".box").hide();
-      $("#box4").show();
+      // $("#box4").show();
 
+      $("#box1").click(function(){
+            // console.log("red box hasbeen clicked");
+            // $("#box2").hide(2000);
+            // $("#box2").toggle();
+            // $("#box2").fadeToggle();
+            // $("#box2").toggleClass("boxLarge orange");
+            // $("#box2").css("width" ,"400px")
+            // $("#box2").css("width":"400px" , "height": "300px");
+      })
 
+      $("#box3").click(function(){
+            // console.log("red box hasbeen clicked");
+            $("#box2").show(2000);
+      })
 
+      $("#box6").dblclick(function() {
+          console.log("Double click is working");
+      });
 
+      $("#box7").mouseover(function() {
+          console.log("mouseover is working");
+      });
 
+      $("#box4").mouseleave(function() {
+            console.log("mouseleave is working");
+      });
 
-
-
-
-
-
-
+      $("#box5").hover(function() {
+          console.log("Hover is working");
+      })
 })
